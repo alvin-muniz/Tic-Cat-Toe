f951852ac259fbd6fbc7003dd4a4c388d225d01b
Alvin Muniz <muniz.alvin@gmail.com>
2021-03-16 17:07:46 -0500

    linked the board array to the browser and simple input command

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
 console.log("script.js loaded")
 
-let board = [
-    ["-","-","-"],
-    ["-","-","-"],
-    ["-","-","-"]
+let mainGame = document.querySelector('main')
+
+const board = [
+    ["1","2","3"],
+    ["4","5","6"],
+    ["7","8","9"]
 ]
+
+displayBoard()
+
+function input(x,y){
+    console.log(board[x][y])
+    board[x][y] = "X"
+}
+
+function displayBoard(){
+    board.forEach(
+        (e, index) => {
+            e.forEach((x,innerIndex) =>{
+                let tile = document.createElement("div");
+                tile.classList.add("tile","location-"+`${index}${innerIndex}`);
+                tile.innerText = index + "" + innerIndex;
+                mainGame.appendChild(tile);
+                }
+            )
+        }
+    )
+}
 

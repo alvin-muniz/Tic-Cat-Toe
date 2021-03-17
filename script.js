e6d479d026cf554bb0897f676ac118139163e557
Alvin Muniz <muniz.alvin@gmail.com>
2021-03-17 12:34:13 -0500

    winning logic for checking a game

--- script.js
+++ script.js
@@ -5,15 +5,33 @@ class Game{
         this.userOne = userOne;
         this.userTwo = userTwo;
         this.currentPlayer = userOne;
+        this.setCurrentPlayer(userOne)
     }
 
     changePlayer(){
         console.log(this.currentPlayer);
         this.currentPlayer = this.currentPlayer == this.userOne ? this.userTwo : this.userOne;
         console.log(this.currentPlayer);
+        document.querySelector("#currentPlayer").innerText = this.currentPlayer.marker;
+    }
+
+    setCurrentPlayer(e){
+        document.querySelector("#currentPlayer").innerText = e.marker
+    }
+
+    setMarkers(x,y){
+        this.userOne.marker = x;
+        this.userTwo.marker = y;
     }
 
 }
+
+class User{
+    constructor(marker){
+        this.marker = marker
+    }
+}
+
 
 
 let mainGame = document.querySelector('main')
@@ -26,7 +44,35 @@ const board = [
 
 displayBoard()
 
-let newGame = new Game("playerOne","playerTwo")
+let userOne = new User("X")
+let userTwo = new User("O")
+
+let newGame = new Game(userOne,userTwo)
+
+function check(){
+    if(board[0][1].innerText == board[0][0].innerText && board[0][0].innerText == board[0][2].innerText) {
+        //top row
+        console.log("top row")
+    }else if (board[1][1].innerText == board[1][0].innerText && board[1][0].innerText == board[1][2].innerText){
+        //middle row
+        console.log("middle row")
+    }else if (board[2][1].innerText == board[2][0].innerText && board[2][0].innerText == board[2][2].innerText){
+        //bottom row
+        console.log("bottom roww")
+    }else if (board[0][1].innerText == board[1][1].innerText && board[0][1].innerText == board[2][1].innerText){
+        //first column
+        console.log("middle down")
+    }else if(board[0][2].innerText == board[1][2].innerText && board[0][2].innerText == board[2][2].innerText){
+
+        console.log("last column")
+    } else if(board[0][0].innerText == board[1][1].innerText && board[0][0].innerText == board[2][2].innerText){
+        console.log("middle diagnol")
+    }else if(board[2][0].innerText == board[1][1].innerText && board[0][0].innerText == board[2][2].innerText){
+        console.log("2nd diagnol")
+    }else{
+
+    }
+}
 
 function input(x,y){
     console.log(board[x][y])
@@ -42,7 +88,10 @@ function displayBoard(){
                 tile.classList.add("tile","location-"+`${index}${innerIndex}`);
                 tile.innerText = index + "" + innerIndex;
                 tile.addEventListener('click',()=>{
-                    tile.innerText = "X";
+                    tile.innerText = newGame.currentPlayer.marker;
+                    console.log(newGame.currentPlayer.marker)
+                    tile.setAttribute("style","pointer-events: none")
+                    newGame.changePlayer()
                 })
                 board[index][innerIndex] = tile;
                 mainGame.appendChild(tile);
@@ -52,8 +101,3 @@ function displayBoard(){
     )
 }
 
-class User{
-    constructor(marker){
-        this.marker = marker
-    }
-}

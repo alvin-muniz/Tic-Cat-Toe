e050ec73e4abf87df50072f0adc8543618517598
Alvin Muniz <muniz.alvin@gmail.com>
2021-03-18 08:49:02 -0500

    added placeholder avatars

--- script.js
+++ script.js
@@ -1,96 +1,92 @@
 console.log("script.js loaded")
 
-
-
-
-class Game{
-    constructor(userOne, userTwo){
+class Game {
+    constructor(userOne, userTwo) {
         this.userOne = userOne;
         this.userTwo = userTwo;
         this.setCurrentPlayer(userOne)
         this.gameCounter = 1;
-        this.board = [
-            ["1","2","3"],
-            ["4","5","6"],
-            ["7","8","9"]
-        ]
 
     }
 
-    startNewGame(){
+    startNewGame() {
         this.createBoard()
     }
 
-    changePlayer(winCondition){
-        // console.log(this.currentPlayer);
-        // console.log(this.currentPlayer);
-        if(winCondition)
-        {
-            console.log("checkWinner in changePlayer hit")
+    changePlayer(winCondition) {
+        this.gameCounter++
+
+        if (winCondition) {
             document.querySelector("h4").innerText = `Winner is ${this.currentPlayer.name}`
         }
-        else{
-            if(this.currentPlayer == this.userTwo)
-            { console.log(this.gameCounter++)}
-
+         else if (this.gameCounter == 10) {
+            document.querySelector("h4").innerText = `It's a cat's game, meow`
+            this.resetGame()
+        }
+        else {
             this.currentPlayer = this.currentPlayer == this.userOne ? this.userTwo : this.userOne;
-            document.querySelector("#currentPlayer").innerText = this.currentPlayer.name + " " + this.currentPlayer.marker;}
-
-
-
-        console.log("Check winner output", winCondition);
-
-
+            document.querySelector("#currentPlayer").innerText = this.currentPlayer.name + " " + this.currentPlayer.marker;
+        }//else
 
     }
 
-    setCurrentPlayer(e){
+    setCurrentPlayer(e) {
         this.currentPlayer = e
         document.querySelector("#currentPlayer").innerText = this.currentPlayer.name + " " + this.currentPlayer.marker;
     }
 
-    setMarkers(x,y){
+    setMarkers(x, y) {
         this.userOne.marker = x;
         this.userTwo.marker = y;
     }
 
-    createBoard(){
+    createBoard() {
         let mainGame = document.querySelector('main')
+        this.board = [
+            ["1", "2", "3"],
+            ["4", "5", "6"],
+            ["7", "8", "9"]
+        ]
         this.board.forEach(
             (e, index) => {
-                e.forEach((x,innerIndex) =>{
+                e.forEach((x, innerIndex) => {
                         let tile = document.createElement("div");
-                        tile.classList.add("tile","location-"+`${index}${innerIndex}`);
-                        tile.innerText = index + "" + innerIndex;
-                        tile.addEventListener('click',(e)=>{
-                            tile.innerText = this.currentPlayer.marker;
-                            console.log(this.currentPlayer.marker)
-                            tile.setAttribute("style","pointer-events: none")
-                            this.changePlayer(this.checkWinner())
-                        })
-                        this.board[index][innerIndex] = tile;
+                        this.setTile(tile, index, innerIndex)
                         mainGame.appendChild(tile);
                     }
-                )
+                )//inner for each loop
             }
-        )
+        )//outer for each loop
     }//end create board
 
-    clearBoard(){
-        this.board.forEach((e, index) =>{
-            e.forEach((x, innerIndex) => {
-                x.innerText = index + "" + innerIndex;
-                x.setAttribute("style","pointer-events: auto")
-            })
-        })
+    setTile(tile, x, y) {
+        tile.classList.add("tile", "location-" + `${x}${y}`);
+        tile.innerText = x + "" + y;
+        tile.addEventListener('click', (e) => {
+            tile.innerText = this.currentPlayer.marker;
+            tile.setAttribute("style", "pointer-events: none")
+            this.changePlayer(this.checkWinner())
+        })//event listener
+        this.board[x][y] = tile;
     }
 
-    checkWinner(){
+
+    resetGame() {
+        this.board.forEach((e, index) => {
+            e.forEach((x, innerIndex) => {
+                x.innerText = index + "" + innerIndex;
+                x.setAttribute("style", "pointer-events: auto")
+            })
+        })
+        this.gameCounter = 0;
+    }
+
+    checkWinner() {
         let winCondition = false
         let set = [
-            [this.board[0][1].innerText, this.board[0][0].innerText, this.board[0][2].innerText ],
-            [this.board[1][1].innerText, this.board[1][0].innerText, this.board[1][2].innerText ],
-            [this.board[2][1].innerText, this.board[2][0].innerText, this.board[2][2].innerText ],
+            [this.board[0][1].innerText, this.board[0][0].innerText, this.board[0][2].innerText],
+            [this.board[1][1].innerText, this.board[1][0].innerText, this.board[1][2].innerText],
+            [this.board[2][1].innerText, this.board[2][0].innerText, this.board[2][2].innerText],
             [this.board[0][1].innerText, this.board[1][1].innerText, this.board[2][1].innerText],
             [this.board[0][2].innerText, this.board[1][2].innerText, this.board[2][2].innerText],
             [this.board[0][0].innerText, this.board[1][1].innerText, this.board[2][2].innerText],
@@ -98,78 +94,25 @@ class Game{
             [this.board[0][0].innerText, this.board[1][0].innerText, this.board[2][0].innerText]
         ]
 
-        console.log()
-        set.forEach((e,index)=>{
+        set.forEach((e, index) => {
             let player = [this.currentPlayer.marker, this.currentPlayer.marker, this.currentPlayer.marker].toString()
-
-            if(e.toString() === player)
-            {
-                // console.log("Yessum! This combination won " + index)
-                this.clearBoard()
+            if (e.toString() === player) {
+                this.resetGame()
                 winCondition = true;
             }
         })
-
         return winCondition
-        //
-        // if(this.board[0][1].innerText == this.board[0][0].innerText && this.board[0][0].innerText == this.board[0][2].innerText) {
-        // // if(set.includes([this.currentPlayer.marker, this.currentPlayer.marker, this.currentPlayer.marker])){
-        //     console.log("winner!")
-        //     this.clearBoard()
-        //     return true;
-        // }else if (this.board[1][1].innerText == this.board[1][0].innerText && this.board[1][0].innerText == this.board[1][2].innerText){
-        //     console.log("middle row")
-        //     this.clearBoard()
-        //     return true;
-        // }else if (this.board[2][1].innerText == this.board[2][0].innerText && this.board[2][0].innerText == this.board[2][2].innerText){
-        //     console.log("bottom row")
-        //     this.clearBoard()
-        //     return true;
-        // }else if (this.board[0][1].innerText == this.board[1][1].innerText && this.board[0][1].innerText == this.board[2][1].innerText){
-        //     console.log("middle down")
-        //     this.clearBoard()
-        //     return true;
-        // }else if(this.board[0][2].innerText == this.board[1][2].innerText && this.board[0][2].innerText == this.board[2][2].innerText){
-        //     console.log("last column")
-        //     this.clearBoard()
-        //     return true;
-        // } else if(this.board[0][0].innerText == this.board[1][1].innerText && this.board[0][0].innerText == this.board[2][2].innerText){
-        //     console.log("middle diagnol")
-        //     this.clearBoard()
-        //     return true;
-        // }else if(this.board[2][0].innerText == this.board[1][1].innerText && this.board[0][0].innerText == this.board[2][2].innerText){
-        //     console.log("2nd diagnol")
-        //     this.clearBoard()
-        //     return true;
-        // }else{
-        //     return false;
-        // }
     }//end check
-
 }
 
-class User{
-    constructor(name, marker){
+class User {
+    constructor(name, marker) {
         this.marker = marker;
         this.name = name;
     }
 }
 
-
-
-let userOne = new User("Cat","X")
-let userTwo = new User("Dog","O")
-
-let newGame = new Game(userOne,userTwo)
-
+let userOne = new User("Cat", "X")
+let userTwo = new User("Dog", "O")
+let newGame = new Game(userOne, userTwo)
 newGame.startNewGame()
-console.log(document.querySelector("#currentPlayer"))
-
-
-// function input(x,y){
-//     console.log(this.board[x][y])
-//     this.board[x][y] = "X"
-//     document.querySelector(`.location-${x}${y}`).innerText = 'X'
-// }
-
-

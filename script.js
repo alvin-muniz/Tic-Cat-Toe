9fbfa6af8a78daa76b03be14446d714c9cafaf34
Alvin Muniz <muniz.alvin@gmail.com>
2021-03-18 15:14:10 -0500

    added session tracking information

--- script.js
+++ script.js
@@ -9,30 +9,65 @@ class Game {
         this.userTwo = userTwo;
         this.setCurrentPlayer(userOne)
         this.gameCounter = 1;
+        this.startNewGame()
     }
 
     startNewGame() {
-        this.createBoard()
+        if(this.gameCounter !== 1){
+            this.gameCounter = 1;
+        }
+
+        console.log("Should equal 1", this.gameCounter)
+
+        // if(!this.board){
+        //     this.createBoard()
+        //     console.log("if no board")
+        // }
+        // else{
+            console.log("else start game")
+            console.log(document.querySelectorAll(".tile"))
+            let nodes = document.querySelectorAll(".tile")
+            nodes.forEach((e, index) => {
+                console.log(index,"this node " + e.innerHTML)
+                e.remove()
+                console.log(index,"this node " + e.innerHTML)
+                console.log(index,"this node " + e.innerHTML)
+                // e.remove()
+            });
+            console.log("remove nodes",nodes.innerHTML)
+            // console.log(nodes);
+            // document.querySelector("main").childNodes.forEach(e => {
+            //     e.remove()
+            // })
+
+            this.createBoard()
+
+        //
+        // }
     }
+
 
     changePlayer(winCondition) {
         this.gameCounter++
+        console.log(this.gameCounter)
 
         if (winCondition) {
             document.querySelector(".winModal").setAttribute("style","display: flex")
             document.querySelector(".winModal h4").innerText = `Winner is ${this.currentPlayer.name}`
-            document.querySelector("#resetGame").addEventListener('click',(e)=>{
-                e.preventDefault()
-                console.log("reset button clicked")
-                document.querySelector(".winModal").setAttribute("style","display: none")
-
-                this.resetGame()
-            })
+            // document.querySelector("#resetGame").addEventListener('click',(e)=>{
+            //     e.preventDefault()
+            //     console.log("reset button clicked")
+            //     document.querySelector(".winModal").setAttribute("style","display: none")
+            //     console.log("Total games played", Session.totalGamesPlayed)
+            //     this.startNewGame
+            //     // Session.totalGamesPlayed.push(new Game(this.userOne, this.userTwo));
+            //   //  this.resetGame()
+            // })
         }
          else if (this.gameCounter == 10) {
-            document.querySelector("h4").innerText = `It's a cat's game, meow`
-            this.resetGame()
-        }
+            document.querySelector(".winModal").setAttribute("style","display: flex")
+            document.querySelector(".winModal h4").innerText = `Cat's Game Meow`
+         }
         else {
             this.currentPlayer = this.currentPlayer == this.userOne ? this.userTwo : this.userOne;
             document.querySelector("#currentPlayer").innerText = this.currentPlayer.name + " " + this.currentPlayer.marker;
@@ -80,26 +115,25 @@ class Game {
     }
 
 
-    resetGame() {
-        this.board.forEach((e, index) => {
-            e.forEach((x, innerIndex) => {
-                x.innerText = index + "" + innerIndex;
-                x.setAttribute("style", "pointer-events: auto")
-            })
-        })
-        this.gameCounter = 0;
-    }
 
     checkWinner() {
         let winCondition = false
         let set = [
+            //top
             [this.board[0][1].innerText, this.board[0][0].innerText, this.board[0][2].innerText],
+            //middle
             [this.board[1][1].innerText, this.board[1][0].innerText, this.board[1][2].innerText],
+            //bottom
             [this.board[2][1].innerText, this.board[2][0].innerText, this.board[2][2].innerText],
+            //middle column
             [this.board[0][1].innerText, this.board[1][1].innerText, this.board[2][1].innerText],
+            //last column
             [this.board[0][2].innerText, this.board[1][2].innerText, this.board[2][2].innerText],
+            //diagnol
             [this.board[0][0].innerText, this.board[1][1].innerText, this.board[2][2].innerText],
-            [this.board[2][0].innerText, this.board[1][1].innerText, this.board[2][2].innerText],
+            //reverse diagnol
+            [this.board[2][0].innerText, this.board[1][1].innerText, this.board[0][2].innerText],
+            //first Column
             [this.board[0][0].innerText, this.board[1][0].innerText, this.board[2][0].innerText]
         ]
 
@@ -124,13 +158,26 @@ class User {
 
 let startButton = document.querySelector("#startGame")
 startButton.addEventListener('click',()=>{
-    let session = new Session();
     let userOne = new User("Cat", "X")
     let userTwo = new User("Dog", "O")
-    let newGame = new Game(userOne, userTwo)
-    newGame.startNewGame()
+    // let newGame = new Game(userOne, userTwo)
+    Session.totalGamesPlayed.push(new Game(userOne, userTwo));
     document.querySelector(".introModal").setAttribute("style","display: none")
 })
+
+document.querySelector("#resetGame").addEventListener('click',(e)=>{
+    e.preventDefault()
+    console.log("reset button clicked")
+    document.querySelector(".winModal").setAttribute("style","display: none")
+    console.log("Total games played", Session.totalGamesPlayed)
+    let userOne = new User("Cat", "X")
+    let userTwo = new User("Dog", "O")
+    Session.totalGamesPlayed.push(new Game(userOne, userTwo));
+    console.log("Session number", Session.totalGamesPlayed.length)
+    // Session.totalGamesPlayed.push(new Game(this.userOne, this.userTwo));
+    // this.resetGame()
+}
+)
 
 
 
